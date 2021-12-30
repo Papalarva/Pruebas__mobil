f9a87c3588aa96784393176377f4c3612da6457d
Papalarva <papalarva1@gmail.com>
2021-12-30 14:51:20 -0700

    Prueba Cesar

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+
+// Desplazamiento de imagenes "Destacados"
+    var splide = new Splide( '.vertical-splide', {
+  direction: 'ttb',
+  type: "loop",
+  height   : '10rem',
+  wheel    : true,
+  arrows: undefined,
+  track: undefined,
+  autoplay: true,
+} );
+
+splide.mount();
